7771daf7787fb107803090a26952710f26607e22
blandygbc <gabrielblandy@gmail.com>
2021-06-24 21:09:50 -0300

    Finalizar a aula 4

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -19,6 +19,11 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   void initState() {
     controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if (controller.status.hasBarcode) {
+        Navigator.pushReplacementNamed(context, "/insert_boleto");
+      }
+    });
     super.initState();
   }
 
@@ -30,14 +35,6 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
 
   @override
   Widget build(BuildContext context) {
-/*     return BottomSheetWidget(
-      title: "Não foi possível identificar um código de barras.",
-      subtitle: "Tente escanear novamente ou digite o código do seu boleto.",
-      primaryLabel: "Escanear novamente",
-      primaryOnPressed: () {},
-      secondaryLabel: "Digitar código",
-      secondaryOnPressed: () {},
-    ); */
     return SafeArea(
       top: true,
       bottom: true,
@@ -94,6 +91,26 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                 secondaryLabel: "Adicionar da galeria",
                 secondaryOnPressed: () {},
               )),
+        ),
+        ValueListenableBuilder<BarcodeScannerStatus>(
+          valueListenable: controller.statusNotifier,
+          builder: (_, status, __) {
+            if (status.hasError) {
+              return BottomSheetWidget(
+                title: "Não foi possível identificar um código de barras.",
+                subtitle:
+                    "Tente escanear novamente ou digite o código do seu boleto.",
+                primaryLabel: "Escanear novamente",
+                primaryOnPressed: () {
+                  controller.getAvailableCameras();
+                },
+                secondaryLabel: "Digitar código",
+                secondaryOnPressed: () {},
+              );
+            } else {
+              return Container();
+            }
+          },
         ),
       ]),
     );

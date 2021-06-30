04a43c63775671cff0210efd79dbc533ee2e83b4
blandygbc <gabrielblandy@gmail.com>
2021-06-30 00:09:26 -0300

    Finalizar a tela de Login

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
@@ -27,17 +29,86 @@ class _LoginPageState extends State<LoginPage> {
             Container(
               width: size.width,
               height: size.height * 0.36,
-              color: AppColors.primary,
+              decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                      center: Alignment.bottomCenter,
+                      radius: 1,
+                      colors: [
+                    Colors.orange.shade200,
+                    AppColors.primary,
+                  ])),
             ),
             Positioned(
                 top: 40,
                 left: 0,
                 right: 0,
-                child: Image.asset(
-                  AppImages.person,
-                  width: 208,
-                  height: 373,
-                )),
+                child: Stack(alignment: AlignmentDirectional.center, children: [
+                  Image.asset(
+                    AppImages.person,
+                    width: 208,
+                    height: 373,
+                  ),
+                  Container(
+                    height: 373,
+                    width: 208,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, 0.4),
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.white.withOpacity(0.0),
+                          Colors.white,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 58,
+                    top: 120,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14),
+                      child: AnimatedCard(
+                        direction: AnimatedCardDirection.right,
+                        duration: Duration(milliseconds: 1900),
+                        curve: Curves.easeOut,
+                        child: Container(
+                          width: 48,
+                          height: 48,
+                          decoration: BoxDecoration(
+                              color: AppColors.secondary,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Icon(
+                            FontAwesomeIcons.fileAlt,
+                            color: AppColors.background,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 59,
+                    top: 202,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14),
+                      child: AnimatedCard(
+                        direction: AnimatedCardDirection.left,
+                        duration: Duration(milliseconds: 2200),
+                        curve: Curves.easeOut,
+                        child: Container(
+                          width: 48,
+                          height: 48,
+                          decoration: BoxDecoration(
+                              color: AppColors.secondary,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Icon(
+                            FontAwesomeIcons.plusSquare,
+                            color: AppColors.background,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ])),
             Positioned(
               bottom: size.height * 0.07,
               left: 0,
